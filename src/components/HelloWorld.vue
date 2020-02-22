9b3e2c9a784ce752afc75bde302ffe3ebfe19aa9
dodolike <1196019060@qq.com>
2020-02-22 19:02:05 +0800

    疫情初步版本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,125 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div ref="checkbox" style="width:900px;height:900px;margin:0 auto;">
+
+    </div>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import 'echarts/map/js/china'
+import jsonp from 'jsonp'
+        var option = {
+            title: {
+                text: 'ECharts 入门示例',
+                subtext:"疫情情况报表",
+                textStyle:{
+                  color:'#cc3113'
+                  },
+                subtextStyle:{
+                  color:'#cc3333',
+                  fontStyle:"italic"
+                }
+            },
+                toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+                  tooltip:{
+                trigger:'item'
+              },
+            series:[{
+              name:'111111',
+              tooltip:{
+                 trigger:'item',
+                  componentType: 'series',
+    // 系列类型
+    seriesType: 'string',
+    // 系列在传入的 option.series 中的 index
+    seriesIndex: 'number',
+    // 系列名称
+    seriesName: 'string',
+    // 数据名，类目名
+    name: 'string'
+              },
+               data:[],
+              type : 'map',
+              map:'china',
+              roam:true,
+              zoom:1.2,
+              label:{
+                show:true,
+              },
+              itemStyle:{
+                areaColor : '#aa3333',
+                borderColor:'aa1111'
+              },
+               emphasis:{
+                 label:{
+                   show:true,
+                   color:'#cc1111'
+                 },
+                 itemStyle:{
+                    areaColor :'#eee'
+                 }
+               }
+              
+            }],
+            visualMap: [{ 
+            type: 'piecewise',
+            show:true,
+            pieces:[
+              {min:10000},
+               {min:1000,max:9999},
+               {min:100,max:999},
+               {min:10,max:99},
+               {min:1,max:9}
+            ],
+            itemWidth:30,
+            itemHeight :30,
+             inRange:{
+               symbol:'circle',
+               color:['blue','red']
+             }
+            // align:'right'
+            
+        }
+    ]
+   
+
+
+        };
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+mounted(){
+  this.myChart = echarts.init(this.$refs.checkbox)
+  this.myChart.setOption(option);
+  this.getData()
+},
+methods:{
+  getData(){
+    jsonp('https://gwpre.sina.cn/interface/fymap2020_data.json',{},(err,data)=>{
+      if(!err){
+        let lists = data.data.list.map(item=>{
+          return {
+            name:item.name,
+            value:item.value
+          }
+        })
+        option.series[0].data = lists
+        console.log(lists)
+        this.myChart.setOption(option);
+      }else{
+        console.log(err)
+      }
+    })
   }
+}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
